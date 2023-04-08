13ac6a453856ccd97a5b5ef114b4d29f2f3c3fbc
KylieGong <87138651+KylieGong@users.noreply.github.com>
2023-04-07 22:10:16 -0400

    made functions

--- newExtension/script.js
+++ newExtension/script.js
@@ -1,15 +1,18 @@
 //script to alter website code
 
-// Select all text elements except for scripts and styles
-const textElements = document.querySelectorAll('*:not(script):not(style)'); 
+//change the font elements in the website, all parameters are strings
+function changeText(fontFamily, fontSize, fontColor) {
+    // Select all text elements except for scripts and styles
+    const textElements = document.querySelectorAll('*:not(script):not(style)'); 
 
-//fonts to choose from
-var fontFamily;
-var fontSize;
-var fontColor;
+    textElements.forEach(el => {
+        el.style.fontFamily = fontFamily; // Change the font family
+        el.style.fontSize = fontSize; // Change the font size
+        el.style.color = fontColor; // Change the text color
+    });
+}
 
-textElements.forEach(el => {
-    el.style.fontFamily = fontFamily; // Change the font family
-    el.style.fontSize = fontSize; // Change the font size
-    el.style.color = fontColor; // Change the text color
-  });
+//change the document background color, parameter in string form
+function changeBgColor(bgColor) {
+    document.body.style.backgroundColor = bgColor;
+}
