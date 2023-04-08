d8761ab1f902231ebe45afb296d37bc657dfb7c8
KylieGong <kyliegong123@gmail.com>
2023-04-08 12:00:47 -0400

    ghjk,l

--- FirefoxExtension/content.js
+++ FirefoxExtension/content.js
@@ -1,6 +1,8 @@
 // Get the current font family of the page
 var currentFont = document.body.style.fontFamily;
 
+document.body.style.fontFamily = "Times New Roman";
+
 // Load the saved font preference, if any, when the content script is loaded
 browser.storage.sync.get('font').then(function(result) {
     var selectedFont = result.font;
