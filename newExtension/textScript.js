87084f7513a9422629f8afe1037de2d3debee8ea
KylieGong <87138651+KylieGong@users.noreply.github.com>
2023-04-07 23:24:44 -0400

    changed to account for headings

--- newExtension/textScript.js
+++ newExtension/textScript.js
@@ -1,13 +1,26 @@
 //script to alter website text
 
-//change the font elements in the website, all parameters are strings
+//change the font elements in the website
+//fontFamily should be entered as a string
+//fontSize should be entered as an int, representing paragraph text size
+//fontColor should be entered as a string
 function changeText(fontFamily, fontSize, fontColor) {
-    // Select all text elements except for scripts and styles
-    const textElements = document.querySelectorAll('*:not(script):not(style)'); 
+    // Select all paragraph and heading elements
+    const paragraphs = document.querySelectorAll('p');
+    const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-    textElements.forEach(el => {
+    //change paragraphs
+    paragraphs.forEach(el => {
         el.style.fontFamily = fontFamily; // Change the font family
-        el.style.fontSize = fontSize; // Change the font size
+        el.style.fontSize = fontSize.toString(); // Change the font size
+        el.style.color = fontColor; // Change the text color
+        console.log("changeText");
+    });
+
+    //change headings
+    headings.forEach(el => {
+        el.style.fontFamily = fontFamily; // Change the font family
+        el.style.fontSize = (1.5*fontSize).toString(); // Change the font size
         el.style.color = fontColor; // Change the text color
         console.log("changeText");
     });
