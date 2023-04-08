a250c9a3e9c3132c86bd74caeda070fbf004ecee
KylieGong <kyliegong123@gmail.com>
2023-04-08 04:48:38 -0400

    gonna ksm

--- newExtension/textScript.js
+++ newExtension/textScript.js
@@ -1,13 +1,22 @@
 //script to alter website text
 
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+
+tab = getCurrentTab()
+
 //change the font elements in the website
 //fontFamily should be entered as a string
 //fontSize should be entered as an int, representing paragraph text size
 //fontColor should be entered as a string
 function changeText(fontFamily, fontSize, fontColor) {
     // Select all paragraph and heading elements
-    const paragraphs = document.querySelectorAll('p');
-    const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+    const paragraphs = tab.querySelectorAll('p');
+    const headings = tab.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
     //change paragraphs
     paragraphs.forEach(el => {
@@ -28,6 +37,6 @@ function changeText(fontFamily, fontSize, fontColor) {
 document.getElementById("myButton").addEventListener("click", changeBgColor);
 
 function changeBgColor() {
-    document.body.style.backgroundColor = "blue"; //can be "blue" or "#FFFFFF"
+    tab.body.style.backgroundColor = "blue"; //can be "blue" or "#FFFFFF"
     alert('PLEASE WORKKKK');
 }
