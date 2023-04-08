98107cf2619b2ba0b1360940e4749f72f009e9e3
KylieGong <kyliegong123@gmail.com>
2023-04-08 05:56:28 -0400

    kmms

--- newExtension/background.js
+++ newExtension/background.js
@@ -1,3 +1,16 @@
-chrome.action.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, {file: "script.js"});
-  });
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: setFontStyleAndSize,
+    });
+});
+
+function setFontStyleAndSize() {
+    const font = prompt("Enter a font style (e.g. Arial, Times New Roman)");
+    const size = prompt("Enter a font size (e.g. 12px, 16pt)");
+
+    if (font && size) {
+        document.body.style.fontFamily = font;
+        document.body.style.fontSize = size;
+    }
+}
