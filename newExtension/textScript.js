d6c452847919c2e85013ff9d125e59884cc0e62b
KylieGong <kyliegong123@gmail.com>
2023-04-08 04:41:16 -0400

    asdf

--- newExtension/textScript.js
+++ newExtension/textScript.js
@@ -4,25 +4,25 @@
 //fontFamily should be entered as a string
 //fontSize should be entered as an int, representing paragraph text size
 //fontColor should be entered as a string
-// function changeText(fontFamily, fontSize, fontColor) {
-//     // Select all paragraph and heading elements
-//     const paragraphs = document.querySelectorAll('p');
-//     const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+function changeText(fontFamily, fontSize, fontColor) {
+    // Select all paragraph and heading elements
+    const paragraphs = document.querySelectorAll('p');
+    const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-//     //change paragraphs
-//     paragraphs.forEach(el => {
-//         el.style.fontFamily = fontFamily; // Change the font family
-//         el.style.fontSize = fontSize; // Change the font size
-//         el.style.color = fontColor; // Change the text color
-//     });
+    //change paragraphs
+    paragraphs.forEach(el => {
+        el.style.fontFamily = fontFamily; // Change the font family
+        el.style.fontSize = fontSize; // Change the font size
+        el.style.color = fontColor; // Change the text color
+    });
 
-//     //change headings
-//     headings.forEach(el => {
-//         el.style.fontFamily = fontFamily; // Change the font family
-//         el.style.fontSize = (1.5*fontSize); // Change the font size
-//         el.style.color = fontColor; // Change the text color
-//     });
-// }
+    //change headings
+    headings.forEach(el => {
+        el.style.fontFamily = fontFamily; // Change the font family
+        el.style.fontSize = (1.5*fontSize); // Change the font size
+        el.style.color = fontColor; // Change the text color
+    });
+}
 
 //change the document background color, parameter in string form
 document.getElementById("myButton").addEventListener("click", changeBgColor);
