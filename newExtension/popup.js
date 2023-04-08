7856c15b75c436c2c140d30d380fa484e4c2bbd7
KylieGong <kyliegong123@gmail.com>
2023-04-08 05:20:04 -0400

    asdf

--- newExtension/popup.js
+++ newExtension/popup.js
@@ -1,14 +1,17 @@
-function applyFont() {
-    let fontStyle = document.getElementById('font-style').value;
-    let fontSize = document.getElementById('font-size').value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: `
-          document.body.style.fontFamily = '${fontStyle}';
-          document.body.style.fontSize = '${fontSize}px';
-        `});
-    });
-  }
+document.addEventListener('DOMContentLoaded', function() {
+    function applyFont() {
+      let fontStyle = document.getElementById('font-style').value;
+      let fontSize = document.getElementById('font-size').value;
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+          tabs[0].id,
+          {code: `
+            document.body.style.fontFamily = '${fontStyle}';
+            document.body.style.fontSize = '${fontSize}px';
+          `});
+      });
+    }
   
-  document.getElementById('apply-font').addEventListener('click', applyFont);
+    document.getElementById('apply-font').addEventListener('click', applyFont);
+  });
+  
