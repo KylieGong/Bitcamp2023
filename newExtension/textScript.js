d33c727a046649c78f899d7dfd49781f8bf6eff7
KylieGong <kyliegong123@gmail.com>
2023-04-08 01:09:46 -0400

    idk

--- newExtension/textScript.js
+++ newExtension/textScript.js
@@ -12,14 +12,14 @@ function changeText(fontFamily, fontSize, fontColor) {
     //change paragraphs
     paragraphs.forEach(el => {
         el.style.fontFamily = fontFamily; // Change the font family
-        el.style.fontSize = fontSize.toString(); // Change the font size
+        el.style.fontSize = fontSize; // Change the font size
         el.style.color = fontColor; // Change the text color
     });
 
     //change headings
     headings.forEach(el => {
         el.style.fontFamily = fontFamily; // Change the font family
-        el.style.fontSize = (1.5*fontSize).toString(); // Change the font size
+        el.style.fontSize = (1.5*fontSize); // Change the font size
         el.style.color = fontColor; // Change the text color
     });
 }
