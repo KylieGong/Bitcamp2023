4f0b4ef21a729f866e0731f7df41b817082f9941
KylieGong <kyliegong123@gmail.com>
2023-04-08 10:45:09 -0400

    gonna kms

---
+++ FirefoxExtension/content.js
@@ -0,0 +1,5 @@
+// Get the current font family of the page
+var currentFont = document.body.style.fontFamily;
+
+// Change the font family of the page to a new font
+document.body.style.fontFamily = "Times New Roman, sans-serif";
