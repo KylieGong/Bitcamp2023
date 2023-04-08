91d135ad1475ccbca811eec2b86abab536e01b47
KylieGong <kyliegong123@gmail.com>
2023-04-08 03:46:41 -0400

    i die

--- Gamification/main.py
+++ Gamification/main.py
@@ -5,5 +5,10 @@ from kivy.uix.image import Image
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 
+class MainApp(app):
+    def build(self):
+        print("I'm gonna kms")
 
- 
+if __name__=="__main__":
+    app = MainApp()
+    app.run()
