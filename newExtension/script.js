cf6a9b9a04d4e69dcb958802726677b584801755
KylieGong <kyliegong123@gmail.com>
2023-04-08 05:38:52 -0400

    asdf

--- newExtension/script.js
+++ newExtension/script.js
@@ -1,4 +1,3 @@
 chrome.action.onClicked.addListener(function(tab) {
     chrome.action.setPopup({tabId: tab.id, popup: 'popup.html'});
-    chrome.tabs.executeScript(tab.id, {file: "script.js"});
   });
