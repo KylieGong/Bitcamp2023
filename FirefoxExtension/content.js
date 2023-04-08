85c7733f1d8118781ca6846a3ddfc603e317ef61
KylieGong <kyliegong123@gmail.com>
2023-04-08 11:55:19 -0400

    test

--- FirefoxExtension/content.js
+++ FirefoxExtension/content.js
@@ -4,6 +4,7 @@ var currentFont = document.body.style.fontFamily;
 // Load the saved font preference, if any, when the content script is loaded
 browser.storage.sync.get('font').then(function(result) {
     var selectedFont = result.font;
+    console.log(selectedFont)
     if (selectedFont) {
       document.body.style.fontFamily = selectedFont;
     }
