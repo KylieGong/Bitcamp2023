9bdb42d637f88de57499fa16c71020b7fb8ee469
KylieGong <kyliegong123@gmail.com>
2023-04-08 12:10:15 -0400

    fghjk

--- FirefoxExtension/content.js
+++ FirefoxExtension/content.js
@@ -1,13 +1,31 @@
 // Get the current font family of the page
-var currentFont = document.body.style.fontFamily;
+var currFont = document.body.style.fontFamily;
 
-document.body.style.fontFamily = "Times New Roman";
+function changeFonts() {
+    const font = "Arial"; // Change this to the font you want to use
+    
+    document.querySelectorAll("*").forEach(element => {
+      element.style.fontFamily = font;
+    });
+  }
 
-// Load the saved font preference, if any, when the content script is loaded
-browser.storage.sync.get('font').then(function(result) {
-    var selectedFont = result.font;
-    console.log(selectedFont)
-    if (selectedFont) {
-      document.body.style.fontFamily = selectedFont;
-    }
+function changeFontSize() {
+    const currentSize = parseInt(window.getComputedStyle(document.body).getPropertyValue("font-size"));
+    const newSize = currentSize + 4; // Increase the font size by 2 pixels
+
+    document.querySelectorAll("*").forEach(element => {
+    element.style.fontSize = newSize + "px";
   });
+}
+  
+changeFonts();
+changeFontSize();
+
+// // Load the saved font preference, if any, when the content script is loaded
+// browser.storage.sync.get('font').then(function(result) {
+//     var selectedFont = result.font;
+//     console.log(selectedFont)
+//     if (selectedFont) {
+//       document.body.style.fontFamily = selectedFont;
+//     }
+//   });
